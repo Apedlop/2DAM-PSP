2fd03bd56a9788633130fcc11edafe3e7306f18d
Apedlop <angelapedreralopez@gmail.com>
2024-09-30 11:54:04 +0200

    30/09/2024, realización Act1_4

--- Tema_1/src/Act1_4/LeerNombre.java
+++ Tema_1/src/Act1_4/LeerNombre.java
@@ -8,15 +8,14 @@ public class LeerNombre {
 
     public static void main(String[] args) throws IOException {
 
-        args = new String[] {"nombre"};
-
-        if (args.length > 0) {
-            System.out.println(Arrays.toString(args));
-            System.exit(1);
-        } else {
+        // Si no le pasamos
+        if (args.length <= 0) {
             System.err.println("Se necesita un programa a ejecutar");
             System.exit(-1);
         }
+
+        System.out.println("Nombre: " + args[0]);
+        System.exit(0);
 
     }
 
